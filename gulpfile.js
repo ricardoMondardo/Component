627befc6a1082666bd23353b386dac159c5c60be
Ricardo <ricardo9300@gmail.com>
2018-06-10 12:27:40 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const fractal = require('@frctl/fractal').create();
+
+fractal.set('project.title', 'Component');
+fractal.set('project.version', 'v1.0');
+fractal.set('project.author', 'Ricardo Brina Mondardo');
+
+fractal.web.set('static.path', __dirname + '/src/public');
+fractal.web.set('builder.dest', 'dist');
+
+fractal.docs.set('path', `${__dirname}/src/docs`);
+fractal.components.set('path', `${__dirname}/src/components`);
+
+const logger = fractal.cli.console;
+
+gulp.task('sass', () => {
+    return gulp.src( `${__dirname}/src/public/scss/styles.scss`)
+    .pipe(sass())
+    .pipe(gulp.dest(`${__dirname}/src/public/css`))
+});
+
+gulp.task('watch', () => {
+    gulp.watch(`${__dirname}/src/public/scss/*.scss`, ['sass'])
+});        
+
+gulp.task('start', function(){
+    const server = fractal.web.server({
+        sync: true
+    });
+    server.on('error', err => logger.error(err.message));
+    return server.start().then(() => {
+        logger.success(`Fractal server is now running at ${server.url}`);
+    });
+});
+
+gulp.task('build', function(){
+    const builder = fractal.web.builder();
+    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+    builder.on('error', err => logger.error(err.message));
+    return builder.build().then(() => {
+        logger.success('Fractal build completed!');
+    });
+});
---
+++ src/public/js/lazy-load-img.js
@@ -0,0 +1,10 @@
+var image = document.images[0];
+var bigImage = document.createElement("img");
+
+/*
+bigImage.onload = function() {
+  image.src = this.src;
+}
+
+bigImage.src = "http://localhost:3000/img/Venice-sm.jpg"
+*/
