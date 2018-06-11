b92c9efdd25cfdd5e08809f4d3cde57788f0218d
Ricardo <ricardo9300@gmail.com>
2018-06-11 02:34:32 +0100

    Doing
    Doing

--- src/public/js/component.js
+++ src/public/js/component.js
@@ -1,1 +1,26 @@
-console.log("Hi from componeent...")
+const card = document.querySelector('.js-animated');
+const title = document.querySelector('.c-title');
+const shadow = document.querySelector('c-card__shadow');
+
+if(card) {
+    card.addEventListener("mouseenter", ()=>{
+        fnShowDetail();
+    });
+    
+    card.addEventListener("mouseleave", ()=>{
+        fnShowClose();
+    });
+    
+    var fnShowDetail = () => {
+        title.classList.add('c-card__title-one--animated');    
+    
+        console.log(shadow);
+    
+        console.log("show details");
+    }
+    
+    var fnShowClose = () => {
+        title.classList.remove('c-card__title-one--animated');
+        console.log("close details");
+    }
+}
