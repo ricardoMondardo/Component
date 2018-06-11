60dcbddaa68f1005c36b9bbe3538dea49592ca61
Ricardo <ricardo9300@gmail.com>
2018-06-11 16:29:14 +0100

    Doing...
    Doing...

--- src/public/js/component.js
+++ src/public/js/component.js
@@ -5,6 +5,9 @@ if(card) {
     const title = document.querySelector('.c-card__section-main');
     const button = document.querySelector('.c-card__button');
     const shadow = document.querySelector('.c-card__shadow');
+    const message = document.querySelector('.c-card__message');
+
+    message.classList.add('c-hidden');
 
     card.addEventListener("mouseenter", ()=>{
         fnShowDetail();
@@ -15,16 +18,19 @@ if(card) {
     });
     
     var fnShowDetail = () => {
-        title.classList.add('c-card--animated-section-main');
-        button.classList.add('c-card--animated-button');
-       
+        title.classList.add('c-card__section-main--animated');
+        button.classList.add('c-card__button--animated');
+        shadow.classList.add('c-card__shadow--animated');
+        message.classList.add('c-card__message--animated');       
         console.log("show details");
     }
     
     var fnShowClose = () => {
-        title.classList.remove('c-card--animated-section-main');
-        button.classList.remove('c-card--animated-button');
-
+        title.classList.remove('c-card__section-main--animated');
+        button.classList.remove('c-card__button--animated');
+        shadow.classList.remove('c-card__shadow--animated');                
+        message.classList.remove('c-card__message--animated');
+        message.classList.add('c-hidden');
         console.log("close details");
     }
 }
