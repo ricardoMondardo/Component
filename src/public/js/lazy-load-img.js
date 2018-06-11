c0ae7a72e9fbe0ef0541baf3b22b37427467024d
Ricardo <ricardo9300@gmail.com>
2018-06-11 18:01:44 +0100

    Doing...
    Doing...

--- src/public/js/lazy-load-img.js
+++ src/public/js/lazy-load-img.js
@@ -1,7 +1,5 @@
-// Get all of the images that are marked up to lazy load
 const images = document.querySelectorAll('.js-lazy-image');
 const config = {
-  // If the image gets within 50px in the Y axis, start the download.
   rootMargin: '50px 0px',
   threshold: 0.01
 };
@@ -9,14 +7,11 @@ const config = {
 let imageCount = images.length;
 let observer;
 
-// If we don't have support for intersection observer, loads the images immediately
 if (!('IntersectionObserver' in window)) {
   loadImagesImmediately(images);
 } else {
-  // It is supported, load the images
   observer = new IntersectionObserver(onIntersection, config);
 
-  // foreach() is not supported in IE
   for (let i = 0; i < images.length; i++) { 
     let image = images[i];
     if (image.classList.contains('js-lazy-image--handled')) {
@@ -27,10 +22,6 @@ if (!('IntersectionObserver' in window)) {
   }
 }
 
-/**
- * Fetchs the image for the given URL
- * @param {string} url 
- */
 function fetchImage(url) {
   return new Promise((resolve, reject) => {
     const image = new Image();
@@ -40,10 +31,6 @@ function fetchImage(url) {
   });
 }
 
-/**
- * Preloads the image
- * @param {object} image 
- */
 function preloadImage(image) {
   const src = image.dataset.src;
   if (!src) {
@@ -53,10 +40,6 @@ function preloadImage(image) {
   return fetchImage(src).then(() => { applyImage(image, src); });
 }
 
-/**
- * Load all of the images immediately
- * @param {NodeListOf<Element>} images 
- */
 function loadImagesImmediately(images) {
   // foreach() is not supported in IE
   for (let i = 0; i < images.length; i++) { 
@@ -66,9 +49,6 @@ function loadImagesImmediately(images) {
   }
 }
 
-/**
- * Disconnect the observer
- */
 function disconnect() {
   if (!observer) {
     return;
@@ -77,37 +57,25 @@ function disconnect() {
   observer.disconnect();
 }
 
-/**
- * On intersection
- * @param {array} entries 
- */
 function onIntersection(entries) {
-  // Disconnect if we've already loaded all of the images
+
   if (imageCount === 0) {
     observer.disconnect();
   }
 
-  // Loop through the entries
   for (let i = 0; i < entries.length; i++) { 
     let entry = entries[i];
     // Are we in viewport?
     if (entry.intersectionRatio > 0) {
       imageCount--;
 
-      // Stop watching and load the image
       observer.unobserve(entry.target);
       preloadImage(entry.target);
     }
   }
 }
-
-/**
- * Apply the image
- * @param {object} img 
- * @param {string} src 
- */
+ 
 function applyImage(img, src) {
-  // Prevent this from being lazy loaded a second time.
   img.classList.add('js-lazy-image--handled');
   img.src = src;
   img.classList.add('fade-in');
