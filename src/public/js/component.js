ef172056d3fc3b4bad966dfd04c609cbd4a13838
Ricardo <ricardo9300@gmail.com>
2018-06-11 14:58:28 +0100

    Doing...
    Doing...

--- src/public/js/component.js
+++ src/public/js/component.js
@@ -1,8 +1,11 @@
 const card = document.querySelector('.js-animated');
-const title = document.querySelector('.c-title');
-const shadow = document.querySelector('c-card__shadow');
 
 if(card) {
+
+    const title = document.querySelector('.c-card__section-main');
+    const button = document.querySelector('.c-card__button');
+    const shadow = document.querySelector('.c-card__shadow');
+
     card.addEventListener("mouseenter", ()=>{
         fnShowDetail();
     });
@@ -12,15 +15,16 @@ if(card) {
     });
     
     var fnShowDetail = () => {
-        title.classList.add('c-card__title-one--animated');    
-    
-        console.log(shadow);
-    
+        title.classList.add('c-card--animated-section-main');
+        button.classList.add('c-card--animated-button');
+       
         console.log("show details");
     }
     
     var fnShowClose = () => {
-        title.classList.remove('c-card__title-one--animated');
+        title.classList.remove('c-card--animated-section-main');
+        button.classList.remove('c-card--animated-button');
+
         console.log("close details");
     }
 }
