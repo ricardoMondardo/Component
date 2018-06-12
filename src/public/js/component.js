eba6b48c32ee2aba6f26b624e5ecc7cd6566af40
Ricardo <ricardo9300@gmail.com>
2018-06-12 16:38:27 +0100

    Doing
    Doing

--- src/public/js/component.js
+++ src/public/js/component.js
@@ -10,6 +10,5 @@ function mOut(obj) {
     obj.querySelector('.c-card__button').classList.remove('c-card__button--animated');;
     obj.querySelector('.c-shadow-background').classList.remove('c-card__shadow--animated');;
     obj.querySelector('.c-card__message').classList.remove('c-card__message--animated');    
-    console.log('mOut');
 }
 
