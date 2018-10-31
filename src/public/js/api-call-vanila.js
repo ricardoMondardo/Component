81aaf1377c91b8e30be135d6d932726753be7c4f
ricardoMondardo <ricardo9300@gmail.com>
2018-10-31 12:26:48 -0300

    Vanila call API
    Vanila call API

---
+++ src/public/js/api-call-vanila.js
@@ -0,0 +1,37 @@
+const result = document.getElementById('result');
+const btn = document.getElementById('btn1');
+
+const Http = new XMLHttpRequest();
+const url='https://jsonplaceholder.typicode.com/posts';
+
+const UNSENT = 0;
+const OPENED = 1;
+const HEADERS_RECEIVED = 2;
+const LOADING = 3;
+const DONE = 4;
+
+var updateUI = function(textUpdate){
+    result.innerHTML += textUpdate;
+};
+
+var callAPi = function(){
+
+    Http.open("GET", url);
+    Http.send();
+    
+    Http.onreadystatechange = function() {
+
+        if(this.readyState = DONE && this.status == 200) {
+            updateUI(Http.responseText);
+        }
+        
+    };
+}
+
+btn.addEventListener("click", callAPi);
+
+
+
+
+
+
